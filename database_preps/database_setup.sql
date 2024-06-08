8c54efe467ee612f302f531a78bc8260966815a7
Marcel Thomas <PriXss@users.noreply.github.com>
2024-06-08 15:51:08 +0200

    adapt table

--- database_preps/database_setup.sql
+++ database_preps/database_setup.sql
@@ -1,7 +1,23 @@
-CREATE DATABASE IF NOT EXISTS productinformation;
-USE productinformation;
+CREATE DATABASE IF NOT EXISTS hotelinformation;
+USE hotelinformation;
 
-CREATE TABLE IF NOT EXISTS products  (
+CREATE TABLE IF NOT EXISTS rooms  (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100),
+    description TEXT,
+    price DECIMAL(10, 2),
+    filename VARCHAR(255)
+);
+
+INSERT INTO rooms (name, description, price, filename) VALUES
+('Premium Suit', 'Schneller, besser teurer, nichts fuer arme Kinder', 109.99, 'bobbycarsport.png'),
+('Classic Bobbycar', 'Schneller, besser teurer, nichts fuer arme Kinder', 109.99, 'bobbycarsport.png'),
+('Needy Bobbycar', 'Schneller, besser teurer, nichts fuer arme Kinder', 109.99, 'bobbycarsport.png'),
+('Fantastic Bobbycar', 'Schneller, besser teurer, nichts fuer arme Kinder', 109.99, 'bobbycarsport.png'),
+('Standard Arm', 'Standard', 19.99, 'bobbycarnormal.png');
+
+
+CREATE TABLE IF NOT EXISTS events  (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(100),
     description TEXT,
